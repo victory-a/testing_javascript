c7265ffab596f9df7108f3eb6ff58f6b2b5ca199
victory-a <victoryasokomeh@gmail.com>
2020-12-28 11:38:22 +0100

    built mini jest

---
+++ fundamentals_of_testing/sum_and_subtract.js
@@ -0,0 +1,16 @@
+const sumAsync = (a, b) => a - b;
+const subtractAsync = (a, b) => a - b;
+
+
+test('sumAsync adds numbers asynchronously', async () => {
+    const result = await sumAsync(3, 7);
+    const expected = 10;
+    expect(result).toBe(expected);
+})
+
+test('subtractAsync subtracts numbers asynchronously', async () => {
+    const result = await subtractAsync(7, 3);
+    const expected = 4;
+    expect(result).toBe(expected);
+}) 
+
